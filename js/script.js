0978f3e77618b32c04c3bf16e2c9de8fe2d297c8
ILYA ZHAVZHAROV <73776583+ilyazhavzharov@users.noreply.github.com>
2024-01-22 14:28:26 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 AOS.init();
+
+// SCROLL
+SmoothScroll({
+  // Время скролла 400 = 0.4 секунды
+  animationTime: 2500,
+  // Размер шага в пикселях
+  stepSize: 75,
+
+  // Дополнительные настройки:
+
+  // Ускорение
+  accelerationDelta: 80,
+  // Максимальное ускорение
+  accelerationMax: 2,
+
+  // Поддержка клавиатуры
+  keyboardSupport: true,
+  // Шаг скролла стрелками на клавиатуре в пикселях
+  arrowScroll: 50,
+
+  // Pulse (less tweakable)
+  // ratio of "tail" to "acceleration"
+  pulseAlgorithm: true,
+  pulseScale: 4,
+  pulseNormalize: 1,
+
+  // Поддержка тачпада
+  touchpadSupport: true,
+});
